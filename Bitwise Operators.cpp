108e33e48a7a3edf13b2fa865ba4c38d33be6408
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-01 17:40:24 +0530

    Add files via upload

---
+++ Bitwise Operators.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+  
+
+  // continue
+
+  // for(int i=0; i<5; i++) {
+  //   cout << i << endl;
+  //   continue;
+  //   cout << "Hello World" << endl;
+  // }
+
+
+  // break
+
+  // for(int i=0; i<5; i++) {
+  //   cout << i << endl;
+  //   break;
+  // }
+  
+  // Increment Operator
+
+  // int a = 10;
+  // cout << (++a) * (a++);
+
+  // // Right Shift Operator
+
+  // int a = 100;
+  // cout << (a >> 1) << endl;
+  // cout << (a >> 2) << endl;
+  // cout << (a >> 3) << endl;
+
+  // // Left Shift Operator
+
+  // int a = 2;
+  // cout << (a << 1) << endl;
+  // cout << (a << 2) << endl;
+  // cout << (a << 3) << endl;
+
+  // Bitwise Operator
+
+  // int a = 5;
+  // int b = -5;
+  // cout << (a ^ b) << endl;
+  
+  
+  // int a = 5;
+  // int b = 10;
+  // cout << (a ^ b) << endl;
+  
+
+  // int a = 5;
+  // int b = 5;
+  // cout << (a ^ b) << endl;
+
+  // int a = 5;
+  // int b = 10;
+
+  // cout << (a & b) << endl;
+  // cout << (a | b) << endl;
+
+  // bool num = 1;
+  // cout << ~num << endl;
+  // cout << (~num) << endl;
+  // cout << ~(num) << endl;
+
+  // int num = 1;
+  // cout << ~num << endl;
+  // cout << (~num) << endl;
+  // cout << ~(num) << endl;
+
+
+  return 0;
+}
