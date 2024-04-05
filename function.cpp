c57ad0c8268533d05ba6666c0cf135cd2ea8aa85
Satyajeet Das Chowdhury <51711970+satyajeetdc@users.noreply.github.com>
2024-04-05 19:57:59 +0530

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -30,9 +30,9 @@ void count(int n) {
 }
 
 int max(int a, int b, int c) {
-  if (a > b && a > c) {
+  if (a >= b && a >= c) {
     return a;
-  } else if (b > a && b > c) {
+  } else if (b >= a && b >= c) {
     return b;
   } else {
     return c;
